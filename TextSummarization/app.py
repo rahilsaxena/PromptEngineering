68838c7e7ae971b9522018a3ef94d8659983fca4
Rahil Saxena <rahilsaxena111@gmail.com>
2024-08-19 22:10:38 +0530

    Text and YT Video Summarization

---
+++ TextSummarization/app.py
@@ -0,0 +1,77 @@
+import validators , streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+#LangSmith Tracking 
+
+
+
+os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_TRACING_V2']="true"
+os.environ['LANGCHAIN_Project']="Text Summerization Using Groq"
+
+### StreamLit App
+
+st.set_page_config(page_title="Langchain:Summarize Text from YT or website")
+st.title("LangcChain: Summarize Text from YT or Website")
+st.subheader('Summarize URL')
+
+
+
+## Get the Groq API and url (YT or website) to be summarized
+
+with st.sidebar:
+    groq_api_key=st.text_input("Groq API Key", value="",type="password")
+
+generic_url=st.text_input("URL",label_visibility="collapsed")
+
+## intialize with Gemma Model and Groq API
+
+llm= ChatGroq(model="Gemma-7b-It",groq_api_key=groq_api_key)
+
+prompt_template= """
+provide summary of following content in 300 words:Content:{text}
+"""
+
+prompt=PromptTemplate(template=prompt_template,input_variables=["text"])
+
+
+
+
+if st.button("Summarize the content from YT or WebSite"):
+    ##Validation of input
+    if not groq_api_key.strip() or not generic_url.strip():
+        st.error("Please provide information to get started")
+    elif not validators.url(generic_url):
+        st.error("Please enter a valid url. It can be YT video or Website Url")
+    
+    else:
+        try:
+            with st.spinner("waiting .."):
+
+                ## Loading youtube data
+                if "youtube.com" in generic_url:
+                   loader= YoutubeLoader.from_youtube_url(generic_url,add_video_info=True)
+                else:
+                    loader=UnstructuredURLLoader(urls=[generic_url],ssl_verify=False)
+
+            doc=loader.load()
+
+
+            ##Chain for Summarization
+            chain=load_summarize_chain(llm,chain_type="stuff",prompt=prompt)
+            output_summary=chain.run(doc)
+
+            st.success(output_summary)
+
+
+        except Exception as e:
+            st.exception(f"Exception:{e}")
+
